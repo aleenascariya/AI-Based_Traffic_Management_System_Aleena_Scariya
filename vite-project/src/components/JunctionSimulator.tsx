01c3517fe74ae0f0e744f7211efb85b6211f2fca
aleenascariya <aleenascariya456@gmail.com>
2026-06-18 10:57:23 +0530

    Add simulator component props

--- vite-project/src/components/JunctionSimulator.tsx
+++ vite-project/src/components/JunctionSimulator.tsx
@@ -1,9 +1,21 @@
-export function JunctionSimulator() {
+import { LaneDirection, Vehicle } from "../types";
+
+interface JunctionSimulatorProps {
+  vehicles: Vehicle[];
+  activeLane: LaneDirection;
+}
+
+export function JunctionSimulator({
+  vehicles,
+  activeLane,
+}: JunctionSimulatorProps) {
   return (
     <div>
       <h2>Traffic Junction Simulator</h2>
 
-      <div>Junction View</div>
+      <p>Active Signal: {activeLane}</p>
+
+      <p>Total Vehicles: {vehicles.length}</p>
     </div>
   );
 }
